77add6706386b1026108ec9545c0177bb5ebd5cb
tanmay <tanmay.murkute@citiuscloud.com>
2026-03-17 10:22:38 +0530

    test pipeline

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.write("Hello from Docker container!\n");
   res.write("Hi, This is my username: @zyfrost");
-  res.end();
+  res.end("Testing CI/CD pipeline with Docker and GitHub Actions.");
 });
 
 server.listen(3000, () => {
